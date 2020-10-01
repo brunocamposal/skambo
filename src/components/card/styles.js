650fede09e85d01a60172b0dcf4ac62ec0bb8f00
Nicolas <nicolas.strike1234@hotmail.com>
2020-10-01 01:02:13 -0300

    feature: added most viewed component rendering products inside carousel

--- src/components/card/styles.js
+++ src/components/card/styles.js
@@ -1,9 +1,34 @@
 import styled from 'styled-components'
 
-export const CardWrapper = styled.div`
+export const Card = styled.div`
+  margin-top:20px;
+  padding:25px;
+  &:hover {
+    
+  }
+ 
+  strong {
+    font-size:14px;
+    font-weight:700;
+    
+  }
+
+  p {
+    color:#545454;
+    font-size:12px;
+    font-weight:400;
+    font-family: 'Roboto', sans-serif;
+  }
+
   img {
     width:182.68px;
     height:197px;
     border-radius:10px;
+    object-position: top;
+    object-fit: cover;
+  }
+  div {
+    margin-top:17px;
+    margin-left: 8px;
   }
 `;
