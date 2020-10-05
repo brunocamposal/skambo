b82c92110793c13d031a57e40033b42efb315838
Nicolas <nicolas.strike1234@hotmail.com>
2020-10-05 18:59:16 -0300

    feature: added user sales, only visually

--- src/pages/user-sales/index.tsx
+++ src/pages/user-sales/index.tsx
@@ -1,17 +1,42 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import * as Styled from './styles'
 import axios from 'axios'
+import jwt_decode from "jwt-decode";
+import { useSelector } from 'react-redux';
+import Card from '../../components/card'
+
+interface stateProps {
+  session: { token: string }
+}
+
+
 
 const UserSales: React.FC = () => {
+  const [userProducts, setUserProducts] = useState([])
+  console.log(userProducts)
+  const token = useSelector((state: stateProps) => state.session.token)
+  const decoded: { sub: string } = jwt_decode(token)
+  console.log('decoded', decoded)
+
   useEffect(() => {
-    axios.get('https://capstone-q2.herokuapp.com/')
-      .then((res) => console.log(res.data))
+    axios.get(`https://capstone-q2.herokuapp.com/products?userId=${2}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then((res) => setUserProducts(res.data))
       .catch(err => console.log(err))
   }, [])
   console.log('user')
   return (
     <Styled.Container>
+      {userProducts && userProducts.map((product: any, key: number) =>
+        (<Card
+          key={key}
+          category={product.category}
+          imgUrl={product.thumbnail}
+          title={product.name}
 
+        />)
+      )}
     </Styled.Container>
   )
 }
