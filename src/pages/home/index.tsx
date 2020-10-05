271dcbc989814462482c8e40ead3066c58cf55cc
paulosantosiii <pauloeduardo_77@hotmail.com>
2020-10-05 17:19:40 -0300

    merging Dev

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import MostViewedProducts from '../../components/most-viewed-products'
+// import MostViewedProducts from '../../components/most-viewed-products'
+import Product from '../../components/product'
 import { Container } from "./styles";
 
 const Home: React.FC = () => {
   return (
     <Container>
-      <MostViewedProducts />
+      <Product />
     </Container>
   );
 };
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -4,7 +4,7 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 950px;
-  margin-top: 80px;
+  margin-top: 120px;
 
   @media (max-width: 480px) {
     margin-top: 30px;
