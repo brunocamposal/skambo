533ee2774c9d36be9c8525d91498c026b21d0626
Nicolas <nicolas.strike1234@hotmail.com>
2020-10-05 18:06:15 -0300

    feature: added products sorted from api on slider

--- src/components/most-viewed-products/index.tsx
+++ src/components/most-viewed-products/index.tsx
@@ -6,59 +6,24 @@ import Slider from 'react-slick';
 import '../../../node_modules/slick-carousel/slick/slick.css';
 import '../../../node_modules/slick-carousel/slick/slick-theme.css';
 import axios from 'axios'
+import { useSelector } from 'react-redux'
 
-// const products = [
-//   {
-//     title: 'Fifa 19',
-//     category: 'Jogos',
-//     imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/61uwGK1Iz0L._AC_SL1000_.jpg'
-//   },
-//   {
-//     title: 'Fifa 19',
-//     category: 'Jogos',
-//     imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/61uwGK1Iz0L._AC_SL1000_.jpg',
-//   },
-//   {
-//     title: 'Fifa 19',
-//     category: 'Jogos',
-//     imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/61uwGK1Iz0L._AC_SL1000_.jpg',
-//   },
-//   {
-//     title: 'Fifa 19',
-//     category: 'Jogos',
-//     imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/61uwGK1Iz0L._AC_SL1000_.jpg',
-//   },
-//   {
-//     title: 'Bicicleta RX890',
-//     category: 'Bicicletas',
-//     imgUrl: 'https://img.olx.com.br/images/32/320073567225211.jpg',
-//   },
-//   {
-//     title: 'Bicicleta RX890',
-//     category: 'Bicicletas',
-//     imgUrl: 'https://img.olx.com.br/images/32/320073567225211.jpg',
-//   },
-//   {
-//     title: 'Bicicleta RX890',
-//     category: 'Bicicletas',
-//     imgUrl: 'https://img.olx.com.br/images/32/320073567225211.jpg',
-//   },
-//   {
-//     title: 'Bicicleta RX890',
-//     category: 'Bicicletas',
-//     imgUrl: 'https://img.olx.com.br/images/32/320073567225211.jpg',
-//   },
-// ];
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImEyQGdtYWlsLmNvbSIsImlhdCI6MTYwMTkyMzc2NSwiZXhwIjoxNjAxOTI3MzY1LCJzdWIiOiIxMiJ9.8b1XmJrkkH-qZP_7u6ynPH1AzJFp3O_dIuRvD27Q0CM"
+interface stateProps {
+  session: { token: string }
+}
+
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImEyQGdtYWlsLmNvbSIsImlhdCI6MTYwMTkyOTYzNywiZXhwIjoxNjAxOTMzMjM3LCJzdWIiOiIxMiJ9.-hMZKStlQVtkyTx0ojIrNktK5SLiiEpbd9bkDyD_RwY"
 const MostViewedProducts = () => {
+  const session = useSelector((state: stateProps) => state.session)
+
   const [products, setProducts] = useState([])
   console.log(products)
   useEffect(() => {
     axios.get('https://capstone-q2.herokuapp.com/products', {
-      headers: { Authorization: `Bearer ${token}` }
+      headers: { Authorization: `Bearer ${session.token}` }
     })
       .then((res) => {
-        const sortedProducts = res.data.sort((a: any, b: any) => {
+        const sortedProducts = res.data.sort((a: { views: string }, b: { views: string }) => {
           return parseInt(b.views) - parseInt(a.views)
         })
         setProducts(sortedProducts)
@@ -111,16 +76,16 @@ const MostViewedProducts = () => {
       <Styled.CarouselContainer>
         <Slider {...settings}>
           {products &&
-            products.map((product: any) => {
-              return <Card views={product.views} title={product.name} category={product.category} imgUrl={product.thumbnail} />;
+            products.map((product: any, key: number) => {
+              return <Card key={key} views={product.views} title={product.name} category={product.category} imgUrl={product.thumbnail} />;
             })}
         </Slider>
       </Styled.CarouselContainer>
       <Styled.Mobile>
         <Styled.MobileContainer>
           {products &&
-            products.map((product: any) => {
-              return <Card views={product.views} title={product.name} category={product.category} imgUrl={product.thumbnail} />;
+            products.map((product: any, key) => {
+              return <Card key={key} views={product.views} title={product.name} category={product.category} imgUrl={product.thumbnail} />;
             })}
         </Styled.MobileContainer>
       </Styled.Mobile>
