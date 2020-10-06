197fbcaba7db41b10df0205f9a8fb467f51ad018
edugraminho <edugraminho@gmail.com>
2020-10-06 10:00:15 -0300

    chore: initial component

---
+++ src/components/change-profile/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Modal } from 'semantic-ui-react';
+
+interface Props {
+  required?: boolean;
+  name: string;
+  type?: string;
+  label?: string;
+  inputPlace: string;
+  inputRef: any;
+  error?: {
+    message?: string;
+  };
+}
+
+const ChangeProfile = () => {
+  return (
+    
+  
+  <div>
+    sdsdsd
+</div>
+)
+};
+
+export default ChangeProfile;
