8358fb05bb6394516e9896f6c1d747ff31df4bcc
paulosantosiii <pauloeduardo_77@hotmail.com>
2020-10-07 21:42:59 -0300

    Mergin from origin Dev

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-// import MostViewedProducts from '../../components/most-viewed-products'
-import Product from '../../components/product'
+import MostViewedProducts from '../../components/most-viewed-products'
+// import Product from '../../components/product'
 import { Container } from "./styles";
-import Product from '../../components/product'
 
 const Home: React.FC = () => {
   return (
     <Container>
-      <Product />
+      <MostViewedProducts />
     </Container>
   );
 };
