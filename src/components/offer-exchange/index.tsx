087be5febd5dc360cf059c1cf831458298207b4d
edugraminho <edugraminho@gmail.com>
2020-10-13 16:36:14 -0300

    made some corrections

--- src/components/offer-exchange/index.tsx
+++ src/components/offer-exchange/index.tsx
@@ -1,31 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modal } from 'semantic-ui-react';
 import { useSelector } from 'react-redux';
 import * as Styled from './styles';
 
-import axios from "axios"
+import axios from 'axios';
 
 const OfferExchange = ({ props }: any) => {
+  const [userProducts, setUserProduct] = useState([]);
+
   const userIdJwt = useSelector((data) => console.log(data));
-  const token = useSelector(({session}: any) => session.token
-   );
+  const token = useSelector(({ session }: any) => session.token);
 
   const onSubmit = () => {
-    // axios.get(`https://capstone-q2.herokuapp.com/products?userId=${userIdJwt}`,{
-    //   headers: { Authorization: `Bearer ${token}` }
-    // })
-    // .then(({data}) => console.log(data))
-
-  }
+    axios
+      .get(`https://capstone-q2.herokuapp.com/products?userId=6`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then(({ data }) => console.log(data)
+      //setUserProduct()
+      );
+  };
 
   return (
     <>
       <Modal open={props}>
         <Modal.Header> Oferecer Produtos para Troca </Modal.Header>
-        <Modal.Content>
-          PlayStation
-          <Styled.ButtonOffer onClick={() => onSubmit()}>Ofertar</Styled.ButtonOffer>
-        </Modal.Content>
+
+        {userProducts.map((produtos, index) => (
+          <Modal.Content key={index}>
+            {produtos}
+            <Styled.ButtonOffer onClick={() => ""}>Ofertar</Styled.ButtonOffer>
+          </Modal.Content>
+        ))}
       </Modal>
     </>
   );
